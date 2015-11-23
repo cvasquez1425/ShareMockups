848f2295198821230a17cfc6bf298029989cdc3e
cvasquez1425 <cvasquez1425@hotmail.com>
2015-11-23 15:25:48 -0500

    Commit SinglePageApplication Project being added to the ShareMockups Solution. CR-ThinkPad 11/23/2015

---
+++ ShareMockups.DomainClasses/TrainingProductManager.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShareMockups.DomainClasses
+{
+    public class TrainingProductManager
+    {
+        public List<TrainingProduct> Get()
+        {
+            List<TrainingProduct> ret = new List<TrainingProduct>();
+
+            //TODO : Add your data access method here.
+            ret = CreateMockData();
+
+            return ret;
+        }
+
+        private List<TrainingProduct> CreateMockData()
+        {
+            List<TrainingProduct> ret = new List<TrainingProduct>();
+
+            ret.Add(new TrainingProduct()
+            {
+                ProductId = 1,
+                ProductName = "Extending Bootstrap with CSS, JavaScrip and JQuery",
+                IntroductionDate = Convert.ToDateTime("10/20/2015"),
+                Url = "http://bit.ly/LSNzc0i",
+                Price = Convert.ToDecimal(29.00)
+            });
+
+            ret.Add(new TrainingProduct()
+            {
+                ProductId = 2,
+                ProductName = "Build Your Own Bootstrap Business Application Template",
+                IntroductionDate = Convert.ToDateTime("10/29/2015"),
+                Url = "http://bit.ly/LSNzc0i",
+                Price = Convert.ToDecimal(29.00)
+            });
+
+            ret.Add(new TrainingProduct()
+            {
+                ProductId = 3,
+                ProductName = "Building Mobile Web Sites Using Web Forms, Bootstrap",
+                IntroductionDate = Convert.ToDateTime("08/28/2014"),
+                Url = "http://bit.ly/LSNzc0i",
+                Price = Convert.ToDecimal(29.00)
+            });
+
+            ret.Add(new TrainingProduct()
+            {
+                ProductId = 4,
+                ProductName = "How To Start and Run A Consulting Business",
+                IntroductionDate = Convert.ToDateTime("09/12/2013"),
+                Url = "http://bit.ly/LSNzc0i",
+                Price = Convert.ToDecimal(29.00)
+            });
+
+            ret.Add(new TrainingProduct()
+            {
+                ProductId = 5,
+                ProductName = "The Many Approaches to XML Processing in .Net Applicaition",
+                IntroductionDate = Convert.ToDateTime("07/22/2013"),
+                Url = "http://bit.ly/LSNzc0i",
+                Price = Convert.ToDecimal(29.00)
+            });
+
+            ret.Add(new TrainingProduct()
+            {
+                ProductId = 6,
+                ProductName = "WPF for the Business Programmer",
+                IntroductionDate = Convert.ToDateTime("06/12/2009"),
+                Url = "http://bit.ly/LSNzc0i",
+                Price = Convert.ToDecimal(29.00)
+            });
+
+            ret.Add(new TrainingProduct()
+            {
+                ProductId = 7,
+                ProductName = "WPF for the Visual Basic Programmer - Part I",
+                IntroductionDate = Convert.ToDateTime("10/20/2015"),
+                Url = "http://bit.ly/LSNzc0i",
+                Price = Convert.ToDecimal(29.00)
+            });
+
+            ret.Add(new TrainingProduct()
+            {
+                ProductId = 8,
+                ProductName = "WPF for the Visual Basic Programmer - Part 2",
+                IntroductionDate = Convert.ToDateTime("2/18/2014"),
+                Url = "http://bit.ly/LSNzc0i",
+                Price = Convert.ToDecimal(29.00)
+            });
+
+            return ret;
+        }
+    }
+}
---
+++ SinglePageApplication/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using ShareMockups.DomainClasses;
+using System.Web.Mvc;
+
+namespace SinglePageApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            TrainingProductManager mgr = new TrainingProductManager();
+
+            return View(mgr.Get());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
